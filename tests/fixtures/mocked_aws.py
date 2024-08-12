a22f4ca8033d0cbccf69d3644bfe9843885bae45
Rhett <rhett.bart@gmail.com>
2024-08-12 10:32:12 -0600

    update test with mocked_aws fixture

---
+++ tests/fixtures/mocked_aws.py
@@ -0,0 +1,36 @@
+import os
+
+import boto3
+from moto import mock_aws
+from pytest import fixture
+
+from tests.consts import TEST_BUCKET_NAME
+
+
+def point_away_from_aws():
+    # This is a function that does not interact with AWS.
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+    return 42
+
+
+@fixture
+def mocked_aws():
+    with mock_aws():
+        # point away from AWS (to be extra careful)
+        point_away_from_aws()
+
+        # create an s3 bucket
+        s3_client = boto3.client("s3")
+        s3_client.create_bucket(Bucket=TEST_BUCKET_NAME)
+
+        yield
+
+        # clean up by deleting the s3 bucket
+        response = s3_client.list_objects_v2(Bucket=TEST_BUCKET_NAME)
+        for obj in response.get("Contents", []):
+            s3_client.delete_object(Bucket=TEST_BUCKET_NAME, Key=obj["Key"])
+        s3_client.delete_bucket(Bucket=TEST_BUCKET_NAME)
--- tests/unit_tests/s3/test__write_objects.py
+++ tests/unit_tests/s3/test__write_objects.py
@@ -1,14 +1,14 @@
+import os
+
 import boto3
+from moto import mock_aws
 
 from files_api.s3.write_objects import upload_s3_object
+from tests.consts import TEST_BUCKET_NAME
 
-TEST_BUCKET_NAME = "test-bucket-rhett"
 
-
-def test__upload_s3_object():
-    # create an s3 bucket
-    s3_client = boto3.client("s3")
-    s3_client.create_bucket(Bucket=TEST_BUCKET_NAME)
+@mock_aws
+def test__upload_s3_object(mocked_aws):
 
     # upload a file to the s3 bucket, with particular content type
     object_key = "test.txt"
@@ -19,16 +19,10 @@ def test__upload_s3_object():
         object_key=object_key,
         file_content=file_content,
         content_type=content_type,
-        s3_client=s3_client,
     )
 
     # check that the file was uploaded with the correct content type
+    s3_client = boto3.client("s3")
     response = s3_client.get_object(Bucket=TEST_BUCKET_NAME, Key=object_key)
     assert response["ContentType"] == content_type
     assert response["Body"].read() == file_content
-
-    # clean up by deleting the s3 bucket
-    response = s3_client.list_objects_v2(Bucket=TEST_BUCKET_NAME)
-    for obj in response.get("Contents", []):
-        s3_client.delete_object(Bucket=TEST_BUCKET_NAME, Key=obj["Key"])
-    s3_client.delete_bucket(Bucket=TEST_BUCKET_NAME)
