a1370a2bb86cb82f3d46a0241548496a26c9d552
Rhett <rhett.bart@gmail.com>
2024-08-12 12:17:22 -0600

    add read_objects.py and tests

--- src/files_api/s3/read_objects.py
+++ src/files_api/s3/read_objects.py
@@ -2,6 +2,9 @@
 
 from typing import Optional
 
+import boto3
+from botocore.exceptions import ClientError
+
 try:
     from mypy_boto3_s3 import S3Client
     from mypy_boto3_s3.type_defs import (
@@ -24,7 +27,19 @@ def object_exists_in_s3(bucket_name: str, object_key: str, s3_client: Optional["
 
     :return: True if the object exists, False otherwise.
     """
-    return
+
+    # Create an S3 client if one is not provided
+    s3_client = s3_client or boto3.client("s3")
+    # Check if the object exists
+    try:
+        s3_client.head_object(Bucket=bucket_name, Key=object_key)
+        return True
+    except ClientError as e:
+        # If the object does not exist, return False
+        if e.response["Error"]["Code"] == "404":
+            return False
+        # Otherwise, raise the exception
+        raise
 
 
 def fetch_s3_object(
@@ -41,7 +56,12 @@ def fetch_s3_object(
 
     :return: Metadata of the object.
     """
-    return
+
+    # Create an S3 client if one is not provided
+    s3_client = s3_client or boto3.client("s3")
+    # Fetch the object metadata
+    response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    return response
 
 
 def fetch_s3_objects_using_page_token(
@@ -62,7 +82,16 @@ def fetch_s3_objects_using_page_token(
         1. Possibly empty list of objects in the current page.
         2. Next continuation token if there are more pages, otherwise None.
     """
-    return
+    # Create an S3 client if one is not provided and set the default max keys
+    s3_client = s3_client or boto3.client("s3")
+    max_keys = max_keys or DEFAULT_MAX_KEYS
+    # List objects in the bucket
+    response = s3_client.list_objects_v2(Bucket=bucket_name, ContinuationToken=continuation_token, MaxKeys=max_keys)
+    # The "Contents" key contains the list of objects.
+    objects = response.get("Contents", [])
+    # The "NextContinuationToken" key contains the token for the next page.
+    next_continuation_token = response.get("NextContinuationToken")
+    return objects, next_continuation_token
 
 
 def fetch_s3_objects_metadata(
@@ -83,4 +112,16 @@ def fetch_s3_objects_metadata(
         1. Possibly empty list of objects in the current page.
         2. Next continuation token if there are more pages, otherwise None.
     """
-    return
+    # Create an S3 client if one is not provided and set the default max keys
+    s3_client = s3_client or boto3.client("s3")
+    max_keys = max_keys or DEFAULT_MAX_KEYS
+    # List objects in the bucket with the given parameters
+    if prefix:
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix, MaxKeys=max_keys)
+    else:
+        response = s3_client.list_objects_v2(Bucket=bucket_name, MaxKeys=max_keys)
+    # The "Contents" key contains the list of objects.
+    objects = response.get("Contents", [])
+    # The "NextContinuationToken" key contains the token for the next page.
+    next_continuation_token = response.get("NextContinuationToken")
+    return objects, next_continuation_token
--- tests/unit_tests/s3/test__read_objects.py
+++ tests/unit_tests/s3/test__read_objects.py
@@ -1,5 +1,11 @@
 """Test cases for `s3.read_objects`."""
 
+import os
+from unittest import mock
+
+import boto3
+from moto import mock_aws
+
 from files_api.s3.read_objects import (
     fetch_s3_objects_metadata,
     fetch_s3_objects_using_page_token,
@@ -8,13 +14,64 @@ from files_api.s3.read_objects import (
 from tests.consts import TEST_BUCKET_NAME
 
 
-def test_object_exists_in_s3(mocked_aws: None): ...
+@mock_aws
+def test_object_exists_in_s3(mocked_aws: None):
+    """Test that the function correctly identifies whether an object exists in an S3 bucket."""
+    s3_client = boto3.client("s3")
+    s3_client.put_object(Bucket=TEST_BUCKET_NAME, Key="test.txt")
+    assert object_exists_in_s3(bucket_name=TEST_BUCKET_NAME, object_key="test.txt") is True
+    assert object_exists_in_s3(bucket_name=TEST_BUCKET_NAME, object_key="nothere.txt") is False
 
 
-def test_pagination(mocked_aws: None): ...
+@mock_aws
+def test_pagination(mocked_aws: None):
+    """Test that the function correctly paginates through the S3 objects."""
+    s3_client = boto3.client("s3")
+    for i in range(10):
+        s3_client.put_object(Bucket=TEST_BUCKET_NAME, Key=f"test{i}.txt")
+
+    # Test pagination for fetch_s3_objects_metadata
+    objects, next_continuation_token = fetch_s3_objects_metadata(bucket_name=TEST_BUCKET_NAME, max_keys=5)
+    assert len(objects) == 5
+    assert objects[0]["Key"] == "test0.txt"
+    assert objects[1]["Key"] == "test1.txt"
+    assert objects[2]["Key"] == "test2.txt"
+    assert objects[3]["Key"] == "test3.txt"
+    assert objects[4]["Key"] == "test4.txt"
+    assert next_continuation_token is not None
+
+    # Test pagination for fetch_s3_objects_using_page_token
+    objects, next_continuation_token = fetch_s3_objects_using_page_token(
+        bucket_name=TEST_BUCKET_NAME, continuation_token=next_continuation_token, max_keys=3
+    )
+    assert len(objects) == 3
+    assert objects[0]["Key"] == "test5.txt"
+    assert objects[1]["Key"] == "test6.txt"
+    assert objects[2]["Key"] == "test7.txt"
+    assert next_continuation_token is not None
+
+    objects, next_continuation_token = fetch_s3_objects_using_page_token(
+        bucket_name=TEST_BUCKET_NAME, continuation_token=next_continuation_token, max_keys=3
+    )
+    assert len(objects) == 2
+    assert objects[0]["Key"] == "test8.txt"
+    assert objects[1]["Key"] == "test9.txt"
+    assert next_continuation_token is None
 
 
-def test_mixed_page_sizes(mocked_aws: None): ...
+@mock_aws
+def test_directory_queries(mocked_aws: None):
+    """Test that the function correctly fetches objects in a directory."""
+    s3_client = boto3.client("s3")
+    s3_client.put_object(Bucket=TEST_BUCKET_NAME, Key="test/test/test1.txt")
+    s3_client.put_object(Bucket=TEST_BUCKET_NAME, Key="test/test2.txt")
+    s3_client.put_object(Bucket=TEST_BUCKET_NAME, Key="test3.txt")
 
+    objects, next_continuation_token = fetch_s3_objects_metadata(bucket_name=TEST_BUCKET_NAME, prefix="test/")
+    assert len(objects) == 2
+    assert objects[0]["Key"] == "test/test/test1.txt"
+    assert objects[1]["Key"] == "test/test2.txt"
 
-def test_directory_queries(mocked_aws: None): ...
+    objects, next_continuation_token = fetch_s3_objects_metadata(bucket_name=TEST_BUCKET_NAME, prefix="test/test/")
+    assert len(objects) == 1
+    assert objects[0]["Key"] == "test/test/test1.txt"
